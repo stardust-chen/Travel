36e6b5105ca9156b161dcbcb99eb1f295a9f7f31
赵晨 <chen1_zhao@saic-gm.com>
2017-12-14 11:24:20 +0800

    日本分支

---
+++ src/pages/japan/Detail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <mt-header fixed title="详细信息">
+      <router-link to="/guangzhou" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
+    <iframe id="webiframe" :src="urls[$route.params.id]" frameborder="0"></iframe>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      urls: [
+        "https://m.ctrip.com/webapp/hybrid/schedule/detail.html?navBarStyle=gray&queryDate=2017-09-08&flightNo=MU9315&queryType=1&fromurl=https://m.ctrip.com/webapp/hybrid/schedule/search.html",
+        ""
+      ]
+    }
+  },
+  created() {
+    document.getElementsByTagName("title")[0].innerText = this.$route.meta.title;
+
+  },
+  // mounted() {
+  //   console.log(document.getElementById("webiframe"))
+  //   document.getElementById("webiframe").onload = function() {
+  //     // if (!navigator.userAgent.match(/iPad|iPhone/i))
+  //     //   return false;
+  //     //获取子iframe
+  //     var iframebody = document.getElementById('webiframe').contentWindow.document.body;
+  //     console.log(iframebody);
+  //     iframebody.style.width = document.body.clientWidth + 'px';
+  //     console.log(document.body.clientWidth)
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.mint-header {
+  background-color: #26a2ff !important;
+  height: 45px !important;
+  font-size: 18px;
+}
+
+
+iframe {
+  width: 375px;
+  height: 103%;
+  margin-top: -3%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import guangzhou from '@/pages/guangzhou/index'
-import Detail from '@/pages/guangzhou/Detail'
+// import Detail from '@/pages/guangzhou/Detail'
+import japan from '@/pages/japan/index'
+// import Detail from '@/pages/japan/Detail'
 
 Vue.use(Router)
 
@@ -10,6 +12,7 @@ export default new Router({
   routes: [
     { path: '/', component: Hello, },
     { path: '/guangzhou', meta: { title: '广州路书' }, component: guangzhou, },
+    { path: '/japan', meta: { title: '2018意大利炮征战霓虹' }, component: japan, },
     // { path: '/Detail/:id', meta: { title: '详细信息' }, component: Detail },
 
   ]
